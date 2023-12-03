57da55fe0a5bf3bd82cada18363edfe42b571dcf
Megan Moore <mmmoore2@buffs.wtamu.edu>
2023-12-03 15:59:20 -0600

    updates

--- myproject/checklistform/views.py
+++ myproject/checklistform/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ExampleForm
 
+def checklistform(request):
+    return HttpResponse("Welcome to CIDM 6395 Project for Megan Moore!")
+
+
+
 from django.template import loader
 
 def checklistform(request):
@@ -11,3 +16,5 @@ def checklistform(request):
         request.POST.getlist(name)))
     return render(request, "studentform2.html", {"method":
 request.method})
+
+
---
+++ myproject/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def checklistform(request):
+    return HttpResponse("Welcome to CIDM 6395 Project for Megan Moore!")
