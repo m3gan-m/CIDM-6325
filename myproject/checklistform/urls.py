b1843652aadd6320070afa186105487fa3c0bb50
Megan Moore <mmmoore2@buffs.wtamu.edu>
2023-12-03 19:25:37 -0600

    static

--- myproject/checklistform/urls.py
+++ myproject/checklistform/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('studentform2/', views.checklistform, name='checklistform'),
     path('welcome/', views.checklistform, name='checklistform'),
     path('', views.welcome_view, name='welcome_view'),
+    path("", TemplateView.as_view(template_name="index.html")),
     #path('form-example/', views.studentform, name='studentform'),
     #path("form-example/", form_example.views.form_example),
     #path('studentform/', studentform.views.studentform)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,12 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('members.urls')),
     path('admin/', admin.site.urls),
     path('', include('studentform.urls')),
     path('', include('checklistform.urls')),
+    path("", TemplateView.as_view(template_name="index.html")),
     #path('', include('forms.urls')),
     #path('', include('myproject.urls'))
 ]
